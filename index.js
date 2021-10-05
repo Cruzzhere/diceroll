174bea60ffdc74904b813024c7fcfc0361241bad
Cruzzhere <89524411+Cruzzhere@users.noreply.github.com>
2021-10-05 12:23:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var n=Math.random();
+n=Math.floor((n*6)+1);
+document.querySelector("img").setAttribute("src","images/dice"+n+".png");
+var i=Math.random();
+i=Math.floor((i*6)+1);
+document.querySelector("img.img2").setAttribute("src","images/dice"+i+".png");
+if(n===i){
+    document.querySelector("h1").innerHTML="Draw";
+}
+if(n>i){
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+if(n<i){
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+}
